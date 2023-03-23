eaff6c68f6cc2795a884ed275f0c18c24691f979
Pariasjulian <125071233+Pariasjulian@users.noreply.github.com>
2023-03-23 14:53:26 -0500

    Creado mediante Colaboratory

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,60 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[],"authorship_tag":"ABX9TyPCSwtqMJhyUFu/r4q7/2UO"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","source":["class calculadora:\n","    def __init__(self, n1, n2):\n","        self.n1 = float(n1)\n","        self.n2 = float(n2)\n","\n","    def sumar(self):\n","        suma = self.n1 + self.n2\n","        print(\"el resultado de la suma es: \", suma)\n","\n","    def restar(self):\n","        resta = self.n1 - self.n2\n","        print(\"el resultado de la resta es: \", resta)\n","\n","    def multiplicar(self):\n","        multiplicacion = self.n1 * self.n2\n","        print(\"el resultado de la multiplicación es: \", multiplicacion)\n","\n","    def dividir(self):\n","        division = self.n1 / self.n2\n","        print(\"el resultado de la división es: \", division)\n","\n","\n","n1 = input(\"ingrese un numero: \")\n","n2 = input(\"ingrese un numero: \")\n","\n","calculadora = calculadora(n1, n2)\n","calculadora.sumar()\n","calculadora.multiplicar()\n","calculadora.restar()\n","calculadora.dividir()"],"metadata":{"colab":{"base_uri":"https://localhost:8080/"},"id":"re_a15zfIYmD","executionInfo":{"status":"ok","timestamp":1679598400040,"user_tz":300,"elapsed":7900,"user":{"displayName":"Julian Andres Salazar Parias","userId":"06747820124389385920"}},"outputId":"1be52934-4e70-438a-aade-d7f936cad790"},"execution_count":3,"outputs":[{"output_type":"stream","name":"stdout","text":["ingrese un numero: 56\n","ingrese un numero: 9.7\n","el resultado de la suma es:  65.7\n","el resultado de la multiplicación es:  543.1999999999999\n","el resultado de la resta es:  46.3\n","el resultado de la división es:  5.773195876288661\n"]}]}]}
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPBCvZt39XreN70k6pfTKbA",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Pariasjulian/Procesamiento_Digital_img/blob/main/calculadora.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#class calculadora:\n",
+        "def __init__(self, n1, n2):\n",
+        "        self.n1 = float(n1)\n",
+        "        self.n2 = float(n2)\n",
+        "\n",
+        "def sumar(self, n1, n2):\n",
+        "        suma = self.n1 + self.n2\n",
+        "        print(\"el resultado de la suma es: \", suma)\n",
+        "\n",
+        "def restar(self, n1, n2):\n",
+        "        resta = self.n1 - self.n2\n",
+        "        print(\"el resultado de la resta es: \", resta)\n",
+        "\n",
+        "def multiplicar(self, n1, n2):\n",
+        "        multiplicacion = self.n1 * self.n2\n",
+        "        print(\"el resultado de la multiplicación es: \", multiplicacion)\n",
+        "\n",
+        "def dividir(self, n1, n2):\n",
+        "        division = self.n1 / self.n2\n",
+        "        print(\"el resultado de la división es: \", division)"
+      ],
+      "metadata": {
+        "id": "re_a15zfIYmD"
+      },
+      "execution_count": 16,
+      "outputs": []
+    }
+  ]
+}
